0481a5ca838641c97d5f7f37c15cc66e992196de
Hayven Baarson <hcbaarson@outlook.com>
2025-04-23 13:57:01 -0400

    REDBLUELEDFADE

--- Fade/Fade.ino
+++ Fade/Fade.ino
@@ -13,20 +13,23 @@
   https://docs.arduino.cc/built-in-examples/basics/Fade/
 */
 
-int led = 9;         // the PWM pin the LED is attached to
+int blueled = 9;      // the PWM pin the LED is attached to
+int redled = 10;        
 int brightness = 0;  // how bright the LED is
 int fadeAmount = 5;  // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
 void setup() {
   // declare pin 9 to be an output:
-  pinMode(led, OUTPUT);
+  pinMode(blueled, OUTPUT);
+  pinMode(redled, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // set the brightness of pin 9:
-  analogWrite(led, brightness);
+  analogWrite(blueled, brightness);
+  analogWrite(redled, brightness);
 
   // change the brightness for next time through the loop:
   brightness = brightness + fadeAmount;
